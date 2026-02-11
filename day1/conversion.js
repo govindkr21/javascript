4095afab4d86f0e690047326e096b009789f1e6c
Govind Kumar Jha <govindkrjha21@gmail.com>
2026-02-12 02:35:08 +0530

    main

---
+++ day1/conversion.js
@@ -0,0 +1,30 @@
+let age = null
+console.log(typeof age);
+console.log(typeof (age));
+ let valueInNumber = Number(age);
+ console.log(valueInNumber);
+ console.log(typeof valueInNumber);
+
+ //"22"= 22 
+ //"22abc"= NaN
+ //null = 0
+ //undefined = NaN
+ //true = 1
+ //false = 0    
+ let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+
+ let num = 33
+ let stringNum = String(num)
+ console.log(stringNum);
+ console.log(typeof stringNum); 
+ // here we can convert number to string using all these methods
+ // "33" = 33
+ // "33abc" = NaN
+ // null = 0
+ // undefined = NaN
+ // true = 1
+ // false = 0    
+ 
