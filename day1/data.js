c7874579779dfa64ad69182e8a3d04f2681e9ae0
Govind Kumar Jha <govindkrjha21@gmail.com>
2026-02-12 01:57:12 +0530

    main

---
+++ day1/data.js
@@ -0,0 +1,26 @@
+
+
+// notes : number 2 to power
+// bigint  bhot bada number ke liye use hota hai 
+// string    => ""
+// boolean => true/false 
+// undefined 
+//null is statement is shows it is not undefinde  it shows value is zero.
+//symbol = unique
+
+//object 
+let name = "Govind";
+let age = 22;
+let isLoggedIn = true;
+
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);// null is object
+
+console.log(typeof undefined);// undefined he ata hai.
+
